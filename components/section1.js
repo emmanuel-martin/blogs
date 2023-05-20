337e8e5eb9d0d0f492da623289521ecef93ede20
Emmanuel Martin <helloiamemmanuel@gmail.com>
2023-05-20 16:47:33 +0530

    bug-fix

--- components/section1.js
+++ components/section1.js
@@ -21,7 +21,7 @@ export default function section1() {
   const bg = {};
 
   return (
-    <section className='py-8' style={bg}>
+    <section className='py-16' style={bg}>
       <div className='container mx-auto md:px-20 w-4/5 sm:max-w-1/2'>
         <Swiper
           slidesPerView={1}
